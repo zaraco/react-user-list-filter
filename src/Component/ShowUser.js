a9cc8c1d072e915400f8dd66c1b2246d13ee4c3e
Zaraco <zahrateymouri90@gmail.com>
2020-04-10 15:53:44 +0200

    table improved and ran

--- src/Component/ShowUser.js
+++ src/Component/ShowUser.js
@@ -1,14 +1,52 @@
 import React, {Component} from 'react';
+import {Table} from "react-bootstrap";
 
 class ShowUser extends Component {
     constructor() {
         super();
+        let items = JSON.parse(localStorage.getItem('items'))
+        this.state = {
+            items: items
+        }
+
+
     }
 
     render() {
+        const {items} = this.state
+        let total = items.length ? items.map((item, i) =>
+
+            <tr key={i}>
+                <td>{item.firstName}</td>
+                <td>{item.lastName}</td>
+                <td style={
+                    item.gender === 'male' ? { background: 'green' } : { background: 'yellow' }
+                }>{item.gender}</td>
+                <td style={
+                    item.state === 'single' ? {background: 'red'} : {background: 'pink'}
+                }>{item.state}</td>
+            </tr>
+
+        ): null
+
         return (
             <>
-                world
+                <Table striped bordered hover>
+                    <thead>
+                    <tr>
+                        <th>FirstName</th>
+                        <th>LastName</th>
+                        <th>Gender</th>
+                        <th>State</th>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+
+                    {total}
+
+                    </tbody>
+                </Table>
             </>
         );
     }
